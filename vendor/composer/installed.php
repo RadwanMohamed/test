78df896c65184a72bd606a076114b25d8c1218b8
RadwanMohamed123 <radwan.mohamed@jobzella.com>
2023-05-08 14:39:08 +0300

    fds

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'php-smpp/php-smpp',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '087cb8999a6099babb1b4704af5704365973d5b5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'php-smpp/php-smpp' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '087cb8999a6099babb1b4704af5704365973d5b5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
